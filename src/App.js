8dbe6614428f3b1145758162f0e3995e45aa2fad
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-11-01 15:17:44 +0000

    search for images using Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
-import './dog.jpg'
 import React, { useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
+
+import './App.css';
 
 function App() {
 
@@ -12,42 +14,30 @@ function App() {
   useEffect(()=>{
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXELBAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
     .then(res=>res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
     .catch(err=> console.log(err))
-  },[]);
+  },[term]);
 
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={require('./dog.jpg')} alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-green-500">Niutek🐶</div> 
-
-     <ul>
-      <li>
-        <strong>🖥️ Views:</strong>4000
-      </li>
-      <li>
-        <strong>🌐 Downloads:</strong>300
-      </li>
-      <li>
-        <strong>❤️ Likes:</strong>222
-      </li>
-     </ul>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text)=> setTerm(text)}/>
+      
+      {!isLoading && images.length ===0 && 
+      <h1 className="text-6xl text-center mx-auto mt-32">No images found 😞</h1>
+      }
+      
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading</h1>
+       :  <div className="grid grid-cols-3 gap-4">
+      {images.map(image => (
+        <ImageCard key={image.id} image={image} />
+      ))}
+      </div>}
     </div>
-     <div className="px-6 py-4 ">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        chiuhuaha
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        animal
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        cute
-      </span>
-     </div>
-    </div>
-
+    
   );
 }
 
