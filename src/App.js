49f63f68c3754b6da6ea52b2bb12dfaff8bff5a4
mpbialoszewski <m.p.bialoszewski@outlook.com>
2023-11-01 14:37:14 +0000

    creating useState and fetching from Pixabay API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import './App.css';
 import './dog.jpg'
+import React, { useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXELBAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res=>res.json())
+    .then(data => console.log(data))
+    .catch(err=> console.log(err))
+  },[]);
+
+
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src={require('./dog.jpg')} alt="" className="w-full" />
       <div className="px-6 py-4">
-        <div class="text-6xl">Henio</div> 
-      </div>
+        <div className="font-bold text-green-500">Niutek🐶</div> 
+
+     <ul>
+      <li>
+        <strong>🖥️ Views:</strong>4000
+      </li>
+      <li>
+        <strong>🌐 Downloads:</strong>300
+      </li>
+      <li>
+        <strong>❤️ Likes:</strong>222
+      </li>
+     </ul>
     </div>
+     <div className="px-6 py-4 ">
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        chiuhuaha
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        animal
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        cute
+      </span>
+     </div>
+    </div>
+
   );
 }
 
